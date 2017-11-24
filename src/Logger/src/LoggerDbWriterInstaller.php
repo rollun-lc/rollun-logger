8771ce2fff76aaacc591890629d2dc634eebac4d
bryakwork <bryakwork@gmail.com>
2017-11-24 18:35:35 +0200

    Fixed installer, now it works as supposed. Also added new feature: installer now checks for right column names in "logs" table

--- src/Logger/src/LoggerDbWriterInstaller.php
+++ src/Logger/src/LoggerDbWriterInstaller.php
@@ -36,57 +36,84 @@ class LoggerDbWriterInstaller extends InstallerAbstract
     }
 
     /**
+     * @param $logDbAdapter
+     * @param $tableLogs
+     * @return array
+     * Checks if 'logs' table exists and if it has right column names
+     *
+     */
+    protected function checkLogsTable($logDbAdapter, $tableLogs)
+    {
+        $result = [];
+        $dbMetadata = Source\Factory::createSourceFromAdapter($logDbAdapter);
+        $tableNames = $dbMetadata->getTableNames();
+        $result["logsTableExist"] = in_array($tableLogs, $tableNames);
+        $columnNames = $dbMetadata->getColumnNames($tableLogs);
+        $rightColumnNames = ["id", "level", "message"];
+        $result["rightColumnsExist"] = in_array($rightColumnNames[0], $columnNames, true)&&
+            in_array($rightColumnNames[1], $columnNames, true)&&
+            in_array($rightColumnNames[2], $columnNames, true);
+        return $result;
+    }
+
+    /**
      * install
      * @return array
      * @throws \Exception
      */
+
     public function install()
     {
         if (constant('APP_ENV') !== 'dev') {
             $this->consoleIO->write('constant("APP_ENV") !== "dev" It has did nothing');
-            return;
+            return [];
         }
-        $this->consoleIO->write('Standart service name for logger db adpter is '
-            . DbLogWriterFactory::LOG_DB_ADAPTER
-        );
-        $logDbAdapterExist = $this->container->has(DbLogWriterFactory::LOG_DB_ADAPTER);
+        //check existence of default DB adapter for DbLogWriter
+        $logDbAdapterServiceMame = DbLogWriterFactory::LOG_DB_ADAPTER;
+        $this->consoleIO->write('Standart service name for Logger`s Db adpter is ' . $logDbAdapterServiceMame);
+        $logDbAdapterExist = $this->container->has($logDbAdapterServiceMame);
+        //if standard adapter is not found, ask to specify existing adapter
         if (!$logDbAdapterExist) {
-
-            $this->consoleIO->write('There is no standart service for logger db adpter', true);
+            $this->consoleIO->write('There is no standart service for Logger`s Db adpter', true);
             $paramName = 'db adapter';
-            $question = 'Enter service name of db adapter for logs: ';
+            $question = 'Enter service name of Db adapter which you want to use for logs: ';
             $defaultValue = 'db';
             $logDbAdapterServiceMame = $this->askParamWithDefault($paramName, $question, $defaultValue);
+            //check if Db adapter was created
             $logDbAdapterExist = $this->container->has($logDbAdapterServiceMame);
             if (!$logDbAdapterExist) {
-                throw new \Exception("db adapter for logger not found! Create it and run this installer again");
-                return;
+                throw new \Exception("Db adapter \"$logDbAdapterServiceMame\" not found! Create it and run this installer again");
             }
         }
-
         $logDbAdapter = $this->container->get($logDbAdapterServiceMame);
+        //check if retrieved adapter is actually a Db adapter
         if (!is_a($logDbAdapter, AdapterInterface::class, true)) {
-            throw new \Exception($logDbAdapterServiceMame . " is not db adapter");
-            return;
+            throw new \Exception($logDbAdapterServiceMame . " is not Db adapter");
         }
-
-        $dbMetadata = Source\Factory::createSourceFromAdapter($logDbAdapter);
-        $tableNames = $dbMetadata->getTableNames();
         $tableLogs = DbLogWriterFactory::LOG_TABLE_NAME;
-        $logsTableExist = in_array($tableLogs, $tableNames);
-        if (!$logsTableExist) {
-            $question = "There is no table \"$tableLogs\" " . PHP_EOL;
-            $question .= "Create it with filds 'id', 'level' and 'message'" . PHP_EOL;
-            $question .= "And type 'y' or 'q' for quit" . PHP_EOL;
+        //check if 'logs' table exists
+        $logsTableValid = $this->checkLogsTable($logDbAdapter, $tableLogs);
+        if (!$logsTableValid["logsTableExist"]) {
+            $question = "Table \"$tableLogs\" was not found." . PHP_EOL;
+            $question .= "Create it with specific fields 'id', 'level' and 'message'." . PHP_EOL;
+            $question .= "And type 'y' if you created the table or 'q' for quit" . PHP_EOL;
             $this->askYesNoQuit($question);
-            $dbMetadata = Source\Factory::createSourceFromAdapter($logDbAdapter);
-            $tableNames = $dbMetadata->getTableNames();
-            $tableLogs = DbLogWriterFactory::LOG_TABLE_NAME;
-            $logsTableExist = in_array($tableLogs, $tableNames);
-            if (!$logsTableExist) {
-                throw new \Exception("table \"$tableLogs\" not found!");
+            //check if user created 'logs' table
+            $logsTableValid = $this->checkLogsTable($logDbAdapter, $tableLogs);
+            if (!$logsTableValid["logsTableExist"]) {
+                throw new \Exception("Table \"$tableLogs\" not found!");
             }
-            return;
+        }
+        //check if "logs" table has the right columns
+        if (!$logsTableValid["rightColumnsExist"]) {
+            $this->consoleIO->write("Table \"$tableLogs\" does not have specific fields 'id', 'level' and 'message'", true);
+            $question = "Check your table and type 'y' to continue or 'q' for quit" . PHP_EOL;
+            $this->askYesNoQuit($question);
+            //check if user fixed the problem
+            $logsTableValid = $this->checkLogsTable($logDbAdapter, $tableLogs);
+            if (!$logsTableValid["rightColumnsExist"]) {
+                throw new \Exception("Table \"$tableLogs\" does not have fields 'id', 'level' and 'message'");
+            }
         }
 
         return [
