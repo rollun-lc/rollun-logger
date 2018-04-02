f70da950152e78259feb2afc0074d3f74b209cc4
victorynox <it.professor02@gmail.com>
2018-04-02 16:40:14 +0300

    fixed namespace fixed params name add lifecycle token serialize and unserialize add parent link and inject. add lifecycle token inject rules add test

--- src/Logger/src/LifeCycleToken.php
+++ src/Logger/src/LifeCycleToken.php
@@ -1,14 +1,21 @@
 <?php
 
 
-namespace rollun\token;
+namespace rollun\logger;
 
 
 use rollun\utils\IdGenerator;
 
-class LifeCycleToken
+class LifeCycleToken implements \Serializable
 {
+    //for system token
+    const KEY_LIFECYCLE_TOKEN = "lifecycle_token";
+    //if sented token not equals to system, system token write with this name
+    const KEY_ORIGINAL_LIFECYCLE_TOKEN = "original_lifecycle_token";
+    //For parent token
     const KEY_PARENT_LIFECYCLE_TOKEN = "parent_lifecycle_token";
+    //if sented parent token not equals to system, system patent token write with this name
+    const KEY_ORIGINAL_PARENT_LIFECYCLE_TOKEN = "original_parent_lifecycle_token";
 
     /**
      * @var string
@@ -16,12 +23,19 @@ class LifeCycleToken
     private $token;
 
     /**
+     * @var self
+     */
+    private $parentToken;
+
+    /**
      * Token constructor.
      * @param string $token
+     * @param LifeCycleToken $parentToken
      */
-    public function __construct(string $token)
+    public function __construct(string $token, LifeCycleToken $parentToken = null)
     {
         $this->token = $token;
+        $this->parentToken = $parentToken;
     }
 
     /**
@@ -48,5 +62,51 @@ class LifeCycleToken
         $idGenerator = new IdGenerator(30);
         $token = new LifeCycleToken($idGenerator->generate());
         return $token;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasParentToken() {
+        return isset($this->parentToken);
+    }
+
+    /**
+     * @return LifeCycleToken
+     */
+    public function getParentToken()
+    {
+        return $this->parentToken;
+    }
+
+    /**
+     * Serialize only own token. Parent token not saved (serialized) and not accessibly after serialization.
+     * String representation of object
+     * @link http://php.net/manual/en/serializable.serialize.php
+     * @return string the string representation of the object or null
+     * @since 5.1.0
+     */
+    public function serialize()
+    {
+        return $this->token;
+    }
+
+    /**
+     * After unserialize token object has changes struct.
+     * Serialized token is becoming parent token, and generate new token for onw lifeCycleToken
+     * Constructs the object
+     * @link http://php.net/manual/en/serializable.unserialize.php
+     * @param string $serialized <p>
+     * The string representation of the object.
+     * </p>
+     * @return void
+     * @since 5.1.0
+     */
+    public function unserialize($serialized)
+    {
+        $this->__construct(
+            self::generateToken()->toString(),
+            new self($serialized)
+        );
     }
 }
--- src/Logger/src/Processor/LifeCycleTokenInjector.php
+++ src/Logger/src/Processor/LifeCycleTokenInjector.php
@@ -4,7 +4,7 @@
 namespace rollun\logger\Processor;
 
 
-use rollun\token\LifeCycleToken;
+use rollun\logger\LifeCycleToken;
 use Zend\Log\Processor\ProcessorInterface;
 
 class LifeCycleTokenInjector implements ProcessorInterface
@@ -32,8 +32,20 @@ class LifeCycleTokenInjector implements ProcessorInterface
      */
     public function process(array $event)
     {
-        if(!isset($event["content"][LifeCycleToken::class])) {
-            $event["content"][LifeCycleToken::class] = $this->token->toString();
+        if (!isset($event[LifeCycleToken::KEY_LIFECYCLE_TOKEN])) {
+            $event[LifeCycleToken::KEY_LIFECYCLE_TOKEN] = $this->token->toString();
+        } elseif (!isset($event['context'][LifeCycleToken::KEY_ORIGINAL_LIFECYCLE_TOKEN]) &&
+            ($this->token->toString() !== $event[LifeCycleToken::KEY_LIFECYCLE_TOKEN])) {
+            $event['context'][LifeCycleToken::KEY_ORIGINAL_LIFECYCLE_TOKEN] = $this->token->toString();
+        }
+        if ($this->token->hasParentToken()) {
+            if (!isset($event[LifeCycleToken::KEY_PARENT_LIFECYCLE_TOKEN])) {
+                $event[LifeCycleToken::KEY_PARENT_LIFECYCLE_TOKEN] = $this->token->getParentToken()->toString();
+            } elseif (!isset($event['context'][LifeCycleToken::KEY_ORIGINAL_PARENT_LIFECYCLE_TOKEN]) &&
+                $this->token->getParentToken()->toString() !== $event[LifeCycleToken::KEY_PARENT_LIFECYCLE_TOKEN]
+            ) {
+                $event['context'][LifeCycleToken::KEY_ORIGINAL_PARENT_LIFECYCLE_TOKEN] = $this->token->getParentToken()->toString();
+            }
         }
         return $event;
     }
