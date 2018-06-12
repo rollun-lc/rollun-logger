9231109e878b8f0aedef95f38655eba6b2210e27
Dmitry Cheva <dmitry.cheva@gmail.com>
2018-06-12 13:10:25 +0300

    create_table_logs.sql issue #24

--- src/create_table_logs.sql
+++ src/create_table_logs.sql
@@ -1,31 +1,23 @@
 
 --
--- База данных: `logs_db`
---
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `logs`
+-- Database `logs_db`
+-- Table `logs`
 --
 
 CREATE TABLE `logs` (
-  `id` varchar(255) NOT NULL,
-  `timestamp` varchar(32) NOT NULL,
-  `level` varchar(32) NOT NULL,
-  `priority` int(11) NOT NULL,
-  `lifecycle_token` varchar(32) NOT NULL,
-  `parent_lifecycle_token` varchar(32),
-  `message` text NOT NULL,
-  `context` text NOT NULL,
-  FOREIGN KEY (`parent_lifecycle_token`) REFERENCES logs(`lifecycle_token`),
-  PRIMARY KEY (`id`),
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
-
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `logs`
---
+	`id` VARCHAR(255) NOT NULL,
+	`timestamp` VARCHAR(32) NOT NULL,
+	`level` VARCHAR(32) NOT NULL,
+	`priority` INT(11) NOT NULL,
+	`lifecycle_token` VARCHAR(32) NOT NULL,
+	`parent_lifecycle_token` VARCHAR(32) NULL DEFAULT NULL,
+	`message` TEXT NOT NULL,
+	`context` TEXT NOT NULL,
+	PRIMARY KEY (`id`),
+	INDEX `parent_lifecycle_token` (`parent_lifecycle_token`),
+	INDEX `lifecycle_token` (`lifecycle_token`),
+	CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`parent_lifecycle_token`) REFERENCES `logs` (`lifecycle_token`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
