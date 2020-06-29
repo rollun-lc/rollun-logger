c057f7133c3db79d3218c26f6282ce482613dc8f
rratsunrollun <r.ratsun.rollun@gmail.com>
2020-06-29 15:21:06 +0300

    Update prometheus

---
+++ bin/prometheus.php
@@ -0,0 +1,23 @@
+<?php
+
+use rollun\logger\LifeCycleToken;
+use Psr\Log\LoggerInterface;
+
+error_reporting(E_ALL ^ E_USER_DEPRECATED ^ E_DEPRECATED);
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+/** @var \Interop\Container\ContainerInterface $container */
+$container = require 'config/container.php';
+
+$lifeCycleToken = LifeCycleToken::generateToken();
+$container->setService(LifeCycleToken::class, $lifeCycleToken);
+
+/** @var LoggerInterface $logger */
+$logger = $container->get(LoggerInterface::class);
+
+$logger->notice('METRICS_GAUGE', ['metricId' => 'metric_1', 'value' => 50, 'groups' => ['group1' => 'val1'], 'labels' => ['red']]);
+
+echo 'Done !';
+die();
